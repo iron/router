0acbfac91cb5ee4741d719be2b58b056d760af92
Michael Contento <michaelcontento@gmail.com>
2015-01-29 19:03:03 +0100

    Fix 'error: unknown `allow` attribute'

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 #![deny(missing_docs, warnings)]
-#![allow(unstable)]
+#![feature(core, std_misc)]
 
 //! `Router` provides a fast router handler for the Iron web framework.
 
 extern crate iron;
 extern crate "route-recognizer" as recognizer;
-
-#[cfg(test)] extern crate test;
 
 pub use router::Router;
 pub use recognizer::Params;
