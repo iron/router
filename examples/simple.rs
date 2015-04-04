a69155c55b699ec94305e249a571a9c166f6f3a7
David Awad <Davidawad64@gmail.com>
2015-04-04 15:44:07 -0400

    updating the example
    Was a little confused on reading this the first time.

--- examples/simple.rs
+++ examples/simple.rs
@@ -10,11 +10,17 @@ use router::{Router};
 
 fn main() {
     let mut router = Router::new();
+    // let's specify a handler for the landing page of our web page. We can respond to all GET requests this way. 
+    // We specify a route as the first argument of router.get and we spefify "handler" as the function to be executed. 
     router.get("/", handler);
+    // We can accept a variable after the root as an argument, in this case its called "query"
+    // for example, "mywebserver.com/rust-iron", query would have the value "rust-iron"
     router.get("/:query", handler);
-
+    
+    //specify iron to run on localhost port 3000
     Iron::new(router).http("localhost:3000").unwrap();
 
+    // this is our handler function to be called when someone send a GET to one of our routes.
     fn handler(req: &mut Request) -> IronResult<Response> {
         let ref query = req.extensions.get::<Router>()
             .unwrap().find("query").unwrap_or("/");
